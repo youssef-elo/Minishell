fcced31569a5ea556e905224be52dee43055a8f9
Hamza Rochd <92557439+HaRo97@users.noreply.github.com>
2024-08-14 04:22:48 +0100

    header file updated

--- minishell.h
+++ minishell.h
@@ -3,14 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yel-ouaz <yel-ouaz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hrochd <hrochd@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 02:45:15 by yel-ouaz          #+#    #+#             */
-/*   Updated: 2024/08/14 02:46:01 by yel-ouaz         ###   ########.fr       */
+/*   Updated: 2024/08/14 03:49:17 by hrochd           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-# define MINISHELL_h
+# define MINISHELL_H
+
+#include <stdio.h>
+
+typedef struct s_env t_env;
+
+struct s_env
+{
+	char *key;
+	char *value;
+	t_env *next;	
+};
+
 
 #endif
