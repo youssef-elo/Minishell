45fca6cc3ffb4f68c8bdfe4d8843d2f156613482
youssef-elo <144487581+youssef-elo@users.noreply.github.com>
2024-10-10 21:54:42 +0100

    echo update

--- builtins/echo.c
+++ builtins/echo.c
@@ -1,14 +1,67 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yel-ouaz <yel-ouaz@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/04 09:54:34 by yel-ouaz          #+#    #+#             */
-/*   Updated: 2024/10/04 10:08:45 by yel-ouaz         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../minishell.h"
 
+int	flag_check(char **args)
+{
+	int i;
+	int j;
+
+	i = 1;
+	while(args[i])
+	{
+		j = 0;
+		if (args[i][j++] != '-' || !args[i][j])
+			return (i);
+		else
+			while(args[i][j])
+			{
+				if (args[i][j] != 'n')
+					return (i);
+				j++;
+			}
+		i++;
+	}
+	if (i == 1)
+		return (i);
+	return (-1);
+}
+
+void ft_putstr_fd(char *str, int fd)
+{
+	while(*str)
+	{
+		write(fd, str, 1);
+		str++;
+	}
+}
+
+void ft_echo(char **args)
+{
+	int start;
+	int nl;
+	
+	start = flag_check(args);
+	nl = start;
+	if (start == -1)
+		return;
+	while(args[start])
+	{
+		ft_putstr_fd(args[start], 1);
+		if (args[start + 1])
+			write(1, " ", 1);
+		start++;
+	}
+	if (nl == 1)
+		write(1, "\n", 1);
+}
+// int main(int argc, char *argv[]){
+// 	if (argc)
+// 	ft_echo(argv);
+// 	//check for write errors diferent exit status
+// }
+
+//echo -nnnnnnnnnnnn... is acceptable 
+//in the -nnnnn as long as its all n its ok 
+//any argument that has -n from the first argument up till there is ni more flags is skipped 
+//for example echo -n -n -nnnnnnnnn -nnnnnnnnnn -nnnnf arguments 
+//the only args that will be printed are the last two the first 4 are considered flags 
+// in the case of echo "" "" "" "" the output should be three spaces wich are the spaces that get printed in between each argument of the echo command
